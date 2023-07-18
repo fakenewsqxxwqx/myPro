7ae1b5870bcf55edab9c86a3a64ea698492e2c8f
YU YANG <yy_17633796552@outlook.com>
2023-07-18 21:04:11 +0800

    spring security配置

--- src/main/java/com/example/mypro/config/securityConfig.java
+++ src/main/java/com/example/mypro/config/securityConfig.java
@@ -1,8 +1,6 @@
 package com.example.mypro.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
